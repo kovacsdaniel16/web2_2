2458df391754c229e965b675e6c7b6010d9567d5
kovacsdaniel16 <kovacsdanieel@gmail.com>
2021-11-26 22:12:54 +0100

    ajax85%

--- felsofoku.php
+++ felsofoku.php
@@ -1,65 +1,65 @@
 <?php
   switch($_POST['op']) {
-    case 'eloado':
+    case 'eloadas':
       $eredmeny = array("lista" => array());
       try {
         $dbh = new PDO('mysql:host=localhost;dbname=web3', 'root', '',
                       array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
-        $stmt = $dbh->query("select id, nev from tudos");
+        $stmt = $dbh->query("select id, cim from eloadas");
 
         while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-              $eredmeny["lista"][] = array("id" => $row['id'], "nev" => $row['nev']);
-              $_SESSION['eredmeny']=$eredmeny;
+              $eredmeny["lista"][] = array("id" => $row['id'], "cim" => $row['cim']);
         }
       }
       catch(PDOException $e) {
       }
-      echo json_encode($eredmeny);
+      echo json_encode($eredmeny); //visszakapja az eredményt
       break;
 
     case 'temakor':
       $eredmeny = array("lista" => array());
       try {
-        $dbh = new PDO('mysql:host=localhost;dbname=web2', 'root', '',
+        $dbh = new PDO('mysql:host=localhost;dbname=web3', 'root', '',
                       array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
-        $stmt = $dbh->prepare("select id, terulet from tudos");
-        $stmt->execute(Array(":id" => $_POST["id"]));
+        $stmt = $dbh->prepare("SELECT tudos.id, nev FROM tudos INNER JOIN kapcsolo on tudosid=tudos.id INNER JOIN eloadas on eloadasid=eloadas.id WHERE eloadas.id= :id"); //második keresés olyan témaköröket keresünk, hol a tudós idja a kulcs
+        $stmt->execute(Array(":id" => $_POST["id"])); //itt az id a JS/post-ból küldött "id"
         while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-              $eredmeny["lista"][] = array("id" => $row['id'], "terulet" => $row['terulet']);
+              $eredmeny["lista"][] = array("id" => $row['id'], "nev" => $row['nev']);
         }
       }
       catch(PDOException $e) {
       }
       echo json_encode($eredmeny);
       break;
-    case 'intezmeny':
+    case 'datum':
       $eredmeny = array("lista" => array());
       try {
-        $dbh = new PDO('mysql:host=localhost;dbname=web2', 'root', '',
+        $dbh = new PDO('mysql:host=localhost;dbname=web3', 'root', '',
                       array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
-        $stmt = $dbh->prepare("select idintezmeny, nev from intezmeny where idvaros = :id");
+        $stmt = $dbh->prepare("SELECT eloadas.id, ido FROM eloadas INNER JOIN kapcsolo ON eloadas.id=eloadasid INNER JOIN tudos ON tudos.id=tudosid WHERE tudos.id = :id");
         $stmt->execute(Array(":id" => $_POST["id"]));
         while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-              $eredmeny["lista"][] = array("id" => $row['idintezmeny'], "nev" => $row['nev']);
+              $eredmeny["lista"][] = array("id" => $row['id'], "date" => $row['ido']);
         }
       }
       catch(PDOException $e) {
       }
       echo json_encode($eredmeny);
       break;
+      
     case 'info':
-      $eredmeny = array("nev" => "", "cim" => "", "tel" => "", "email" => "");
+      $eredmeny = array("eloado" => "", "eloadas" => "", "temakor" => "", "datum" => "");
       try {
-        $dbh = new PDO('mysql:host=localhost;dbname=web2', 'root', '',
+        $dbh = new PDO('mysql:host=localhost;dbname=web3', 'root', '',
                       array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
-        $stmt = $dbh->prepare("select nev, cim, telefon, email from intezmeny where idintezmeny = :id");
+        $stmt = $dbh->prepare("SELECT nev, cim, terulet, ido FROM eloadas INNER JOIN kapcsolo ON eloadas.id=eloadasid INNER JOIN tudos ON tudos.id=tudosid WHERE eloadas.ido= :id");
         $stmt->execute(Array(":id" => $_POST["id"]));
         if($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-              $eredmeny = array("nev" => $row['nev'], "cim" => $row['cim'], "tel" => $row['telefon'], "email" => $row['email']);
+              $eredmeny = array("nev" => $row['nev'], "cim" => $row['cim'], "temakor" => $row['terulet'], "datum" => $row['email']);
         }
       }
       catch(PDOException $e) {
@@ -67,4 +67,5 @@
       echo json_encode($eredmeny);
       break;
   }
+  
 ?>
