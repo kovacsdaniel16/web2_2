1850ca4a7b521e9d5866898980415a6815086da4
kovacsdaniel16 <kovacsdanieel@gmail.com>
2021-11-29 09:01:23 +0100

    link

--- views/nyitolap_main.php
+++ views/nyitolap_main.php
@@ -1,8 +1,10 @@
 
 <h3>A műsorról</h3>
-<p id="magyarazat">&nbsp&nbsp&nbspA Mindentudás Egyeteme egy 2002. szeptember 16-án elindult ismeretterjesztő vállalkozás. A 10. szemeszter befejezése után három évvel, 2011. január 15-én Mindentudás Egyeteme <em>2.0</em> néven folytatódott a népszerű sorozat.</p><br>
+<p id="magyarazat">&nbsp&nbsp&nbspA <b>Mindentudás Egyeteme</b> egy 2002. szeptember 16-án elindult ismeretterjesztő vállalkozás. A 10. szemeszter befejezése után három évvel, 2011. január 15-én Mindentudás Egyeteme <em>2.0</em> néven folytatódott a népszerű sorozat.</p><br>
 
 <h3>Az eredeti ötlet</h3>
-<p>&nbsp&nbsp&nbspA francia Yves Michaud műtörténész, médiakutató 1998-ban hozta létre a l'Université de tous les savoirs című programot. A vállalkozásnak nagy sikere volt. Eszmeiségében a felvilágosodáskori francia nagy Enciklopédiához kapcsolódott: "hogyan lehetne áttekinteni és az érdeklődő közönség számára is hozzáférhetővé tenni korunk legmagasabb szintű tudományos ismereteit, a kortárs tudomány legfőbb dilemmáit?"</p><br>
+<p>&nbsp&nbsp&nbspA francia <b>Yves Michaud</b> műtörténész, médiakutató 1998-ban hozta létre a l'Université de tous les savoirs című programot. A vállalkozásnak nagy sikere volt. Eszmeiségében a felvilágosodáskori francia nagy Enciklopédiához kapcsolódott: "hogyan lehetne áttekinteni és az érdeklődő közönség számára is hozzáférhetővé tenni korunk legmagasabb szintű tudományos ismereteit, a kortárs tudomány legfőbb dilemmáit?"</p><br>
 
+<p><a class="main" href="https://mindentudas.hu/
+" target="_blank">Látogassa meg a Mindentudás Egyeteme 2.0 honlapját!</a><br><br><br></p>
 
